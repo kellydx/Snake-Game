d0c94666270a1e61557d8cc90e5e9d32b0b8b640
Kelly  Duong <kelly1234@gmail.com>
2017-03-01 22:22:40 -0500

    p5js sketch

---
+++ sketch.js
@@ -0,0 +1,61 @@
+mouse
+var snake;
+var grid = 20;
+var mouse;
+var eat;
+var gameOver;
+var theme;
+var level =3;
+
+function preload() {
+  eat = loadSound("eat.mp3");
+  gameOver = loadSound("die.mp3");
+  theme = loadSound("theme1.mp4");
+}
+
+function setup() {
+  createCanvas(600, 600);
+  theme.play();
+  theme.playMode('restart');
+  snake = new Snake();
+  frameRate(level);
+  mouseLocation();
+}
+
+function mouseLocation() {
+  var col = floor(width/grid);
+  var row = floor(height/grid);
+  mouse = createVector(floor(random(col)), floor(random(row)));
+  mouse.mult(grid);
+}
+
+function draw() {
+  background(51);
+
+  if (snake.eat(mouse)) {
+    eat.play();
+    level=level+3;
+    frameRate(level);
+    mouseLocation();
+  }
+
+  snake.die();
+  snake.update();
+  snake.show();
+  snake.restartMusic();
+
+  fill(255, 0, 100);
+  rect(mouse.x, mouse.y, grid, grid);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    snake.turn(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.turn(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.turn(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    snake.turn(-1, 0);
+  }  
+}
